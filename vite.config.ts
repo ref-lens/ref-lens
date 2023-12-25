03bd9f619ffd5844c930eb5e2cce622cc46c5e9d
Gabe Scholz <github@gabe.pizza>
2023-12-24 22:25:01 -0800

    Move src to lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
+import path from "node:path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   test: {
     globals: true,
   },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "lib/main.ts"),
+      name: "RefLens",
+      fileName: (format) => `main.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
+  plugins: [
+    dts({
+      entryRoot: path.resolve(__dirname, "lib"),
+    }),
+  ],
 });
